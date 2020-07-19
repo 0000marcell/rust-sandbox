f2bdde16d6594642a1b32fe3f80be1721cbc68b2
____marcell <0000marcell@gmail.com>
2020-07-19 22:53:35 +0000

    ownership with burrow and functions

--- general/src/main.rs
+++ general/src/main.rs
@@ -1,6 +1,28 @@
-fn main() {
+fn failing_example() {
     let x = String::from("marcell");
-    let y = x;
+
+
+    // we need to burrow here so we can use the string afterwards
+    moved_here(x);
 
     println!("will fail {} ", x); // the value was moved here
+}
+
+fn passing_example() {
+    let x = String::from("marcell");
+
+
+    // we need to burrow here so we can use the string afterwards
+    moved_here(&x);
+
+    println!("will fail {} ", x); // the value was moved here
+}
+
+
+fn main() {
+    passing_example(); 
+}
+
+fn moved_here(x: &String) {
+    println!("will fail {} ", x);
 }
